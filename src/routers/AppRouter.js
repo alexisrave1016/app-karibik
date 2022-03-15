1a8e6044a6cd3ab2caa37901ae2ff09319a84df8
alexisRave <alexis9876@hotmail.com>
2022-03-14 21:39:33 -0500

    inicio de prueba

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    HashRouter as Router,
+    Switch
+} from "react-router-dom";
+
+
+const AppRouter = () => {
+  return (
+   <Router>
+       <Switch>
+           
+       </Switch>
+   </Router>
+  )
+}
+
+export default AppRouter
